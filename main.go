4b8cc0fb631aa4f1622014390d16cdfafea4f62e
yamotonalds <yamotonalds@gmail.com>
2017-07-23 16:32:37 +0900

    Echo loop from stdin

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Println("hello golang")
+	var word string
+	for {
+		_, err := fmt.Scanln(&word)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Fprintf(os.Stderr, "単語が読み込めませんでした: %v\n", err)
+			}
+			break
+		}
+
+		fmt.Println(word)
+	}
 }
