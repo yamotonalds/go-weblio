3b90951f4a14f32020c5212511db4f1417a8f674
yamotonalds <yamotonalds@gmail.com>
2017-07-24 01:42:47 +0900

    Use pipeline pattern

--- main.go
+++ main.go
@@ -9,27 +9,64 @@ import (
 )
 
 func main() {
-	var word string
-	for {
-		_, err := fmt.Scanln(&word)
-		if err != nil {
-			if err != io.EOF {
-				fmt.Fprintf(os.Stderr, "単語が読み込めませんでした: %v\n", err)
-			}
-			break
-		}
+	inputWords := readWords()
+	translatedWords := translateWords(inputWords)
 
-		doc, err := goquery.NewDocument("http://ejje.weblio.jp/content/" + word)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s: 翻訳ページが読み込めませんでした(%v)\n", word, err)
-		}
-
-		contentExplanation := doc.Find(".content-explanation").First()
-		if contentExplanation == nil {
-			fmt.Fprintf(os.Stderr, "%s: 翻訳が見つかりませんでした", word)
-		}
-		translated := contentExplanation.Text()
-
+	for translated := range translatedWords {
 		fmt.Println(translated)
 	}
+}
+
+func readWords() chan string {
+	inputWords := make(chan string, 10)
+
+	go func() {
+		for {
+			var word string
+			_, err := fmt.Scanln(&word)
+			if err != nil {
+				if err != io.EOF {
+					fmt.Fprintf(os.Stderr, "単語が読み込めませんでした: %v\n", err)
+				}
+				break
+			}
+
+			inputWords <- word
+		}
+		close(inputWords)
+	}()
+
+	return inputWords
+}
+
+func translateWords(inputWords chan string) chan string {
+	translatedWords := make(chan string, 10)
+
+	go func() {
+		for word := range inputWords {
+			translated, err := translate(word)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "翻訳エラー: %v\n", err)
+				continue
+			}
+			translatedWords <- translated
+		}
+		close(translatedWords)
+	}()
+
+	return translatedWords
+}
+
+func translate(word string) (string, error) {
+	doc, err := goquery.NewDocument("http://ejje.weblio.jp/content/" + word)
+	if err != nil {
+		return "", fmt.Errorf("%s: 翻訳ページが読み込めませんでした(%v)", word, err)
+	}
+
+	contentExplanation := doc.Find(".content-explanation").First()
+	if contentExplanation == nil {
+		return "", fmt.Errorf("%s: 翻訳が見つかりませんでした", word)
+	}
+
+	return contentExplanation.Text(), nil
 }
